47f59f8d6e0b58c5d86c3a1ca9360a2b14a399b8
Ke Duong <kelyduong@gmail.com>
2023-11-01 23:51:11 +1100

    prepare for github question

--- client/src/Bear.store.ts
+++ client/src/Bear.store.ts
@@ -1,35 +1,27 @@
-import type {} from "@redux-devtools/extension"; // required for devtools typing
 import { create } from "zustand";
-import { devtools, persist } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
 interface BearState {
   bears: number;
   increase: (by: number) => void;
 }
 
-const useBearStore = create<
-  BearState,
-  [["zustand/devtools", never], ["zustand/persist", BearState]]
->(
-  devtools(
-    persist(
-      (set) => ({
-        bears: 0,
-        increase: (by) => set((state) => ({ bears: state.bears + by })),
-      }),
-      { name: "bearStore" }
-    )
+const useBearStore = create<BearState, [["zustand/persist", BearState]]>(
+  persist(
+    (set) => ({
+      bears: 0,
+      increase: (by) => set((state) => ({ bears: state.bears + by })),
+    }),
+    { name: "bearStore" }
   )
 );
 
 const useBearStore2 = create<BearState>()(
-  devtools(
-    persist(
-      (set) => ({
-        bears: 0,
-        increase: (by) => set((state) => ({ bears: state.bears + by })),
-      }),
-      { name: "bearStore" }
-    )
+  persist(
+    (set) => ({
+      bears: 0,
+      increase: (by) => set((state) => ({ bears: state.bears + by })),
+    }),
+    { name: "bearStore" }
   )
 );
