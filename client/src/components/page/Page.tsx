a40bee171f9c4131cf4f2e1337daf06433347c2b
Ke Duong <kelyduong@gmail.com>
2023-11-04 19:54:26 +1100

    more responsive assessment page --> use overflow auto

--- client/src/components/page/Page.tsx
+++ client/src/components/page/Page.tsx
@@ -1,6 +1,6 @@
 import { type ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
-import NavBar from "./Navbar";
+import NavigationBar from "./NavigationBar";
 import { PageLocation } from "./PageLocation";
 import { ModeToggle } from "./Theme/ModeToggle";
 import { ThemeProvider } from "./Theme/ThemeProvider";
@@ -21,16 +21,16 @@ const Page = ({
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex h-screen">
+      <div className="flex w-screen h-screen">
         <div className="w-fit">
-          <NavBar />
+          <NavigationBar />
         </div>
-        <div className="flex flex-col w-full mx-4 mt-4">
+        <div className="flex flex-col w-full mx-2 mt-4 lg:mx-4">
           <div className="flex justify-between w-full">
             <h1>{pageTitle}</h1>
             <ModeToggle />
           </div>
-          <main className="h-full overflow-y-scroll">{children}</main>
+          <main className="w-full h-full pb-4 overflow-y-auto">{children}</main>
         </div>
       </div>
     </ThemeProvider>
