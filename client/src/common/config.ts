0ab52c0f091f7cfff9fddd19fc8d69deab3e8c0c
Ke Duong <kelyduong@gmail.com>
2023-11-28 23:12:55 +1100

    add config & auth0 provider

---
+++ client/src/common/config.ts
@@ -0,0 +1,29 @@
+type configType = {
+  auth0: {
+    domain: string;
+    clientId: string;
+  };
+};
+
+const getConfig = (hostname: string): configType => {
+  console.log("here", hostname);
+  switch (hostname) {
+    case "localhost":
+    case "192.168.1.103":
+      return {
+        auth0: {
+          domain: "dev-bigbrain.au.auth0.com",
+          clientId: "mEeuPnXlIxWfgSDAiAz5h36K0pgHn0ch",
+        },
+      } satisfies configType;
+    default:
+      return {
+        auth0: {
+          domain: "",
+          clientId: "",
+        },
+      } satisfies configType;
+  }
+};
+
+export const config = getConfig(window.location.hostname);
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
+import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+
+import { config } from "./common/config.ts";
 import "./index.css";
 
+const { auth0 } = config;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain={auth0.domain}
+    clientId={auth0.clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
