b7fd9ba6f4996b24dea34d1aa6466e28d8bd36dc
Ke Duong <kelyduong@gmail.com>
2023-12-07 00:51:28 +1100

    add new env & add more stuff for profile

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,18 +1,17 @@
-import { useAuth, useUser } from "@clerk/clerk-react";
+import { UserButton, useUser } from "@clerk/clerk-react";
 import Page from "../../components/page/Page";
 import type { UserProfileMetadata } from "./UserProfile";
 
 const Profile = () => {
-  const { isLoaded, userId, sessionId } = useAuth();
-  const { user } = useUser();
+  const { isLoaded, isSignedIn, user } = useUser();
 
   const userMetaData = user?.publicMetadata as UserProfileMetadata;
   return (
     <Page pageTitle="Profile">
-      {isLoaded && userId && (
+      {isLoaded && isSignedIn && (
         <div>
-          Hello, {userId}:{user?.fullName} your current active session is{" "}
-          {sessionId}. You are from {userMetaData.school}
+          Hello, {user?.fullName}. You are from {userMetaData.school}
+          <UserButton />
         </div>
       )}
     </Page>
