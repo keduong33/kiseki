31cdbe26bd4d8135ff0330f8032bfa6bb6c06c13
Ke Duong <kelyduong@gmail.com>
2023-12-09 01:09:09 +1100

    redirect to dashboard after logout

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,17 +1,19 @@
 import { UserButton, useUser } from "@clerk/clerk-react";
 import Page from "../../components/page/Page";
+import { PageLocation } from "../../components/page/PageLocation";
 import type { UserProfileMetadata } from "./UserProfile";
 
 const Profile = () => {
   const { isLoaded, isSignedIn, user } = useUser();
 
   const userMetaData = user?.publicMetadata as UserProfileMetadata;
+
   return (
     <Page pageTitle="Profile">
       {isLoaded && isSignedIn && (
         <div>
           Hello, {user?.fullName}. You are from {userMetaData.school}
-          <UserButton />
+          <UserButton afterSignOutUrl={PageLocation.Dashboard} />
         </div>
       )}
     </Page>
