0c07ed3e271c582841c523411e9314594e30bcb7
Ke Duong <kelyduong@gmail.com>
2023-12-14 19:31:13 +1100

    (be) new endpoint

--- backend/src/common/neo4jDriver.ts
+++ backend/src/common/neo4jDriver.ts
@@ -1,16 +1,17 @@
 import { Driver, auth, driver } from "neo4j-driver";
 import { getBackendConfig } from "./config";
+import { safeError, safeResult, type SafeResponse } from "./safeResponse";
 
-export const initNeo4jDriver = (): Driver | undefined => {
+export const initNeo4jDriver = (): SafeResponse<Driver> => {
   const { neo4j } = getBackendConfig();
   if (!neo4j) {
     console.warn("Failed in Neo4jDriver", "Invalid environment");
-    return;
+    return safeError({ message: "Cannot connect to Neo4j Database" });
   }
   const neo4jDriver = driver(
     neo4j.uri,
     auth.basic(neo4j.username, neo4j.password)
   );
 
-  return neo4jDriver;
+  return safeResult(neo4jDriver);
 };
