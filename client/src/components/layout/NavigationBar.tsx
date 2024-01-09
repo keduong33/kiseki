2151d85e1313ddec4905f4e4aea7f6d614297bfe
Ke Duong <kelyduong@gmail.com>
2024-01-09 23:19:33 +1100

    (fe) remove radix navigation menu

--- client/src/components/layout/NavigationBar.tsx
+++ client/src/components/layout/NavigationBar.tsx
@@ -1,30 +1,26 @@
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuList,
-} from "@radix-ui/react-navigation-menu";
-
 import { Book1, Bookmark, Chart1, Firstline, People } from "iconsax-react";
 import { LayoutDashboard } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
 import { PageLocation } from "../../../../types/PageLocation";
-import { NavigationMenuLink } from "../shadcn/ui/navigation-menu";
 
 type navBarLink = {
   title: string;
   destination: PageLocation;
   icon?: React.ReactNode;
+  disabled?: boolean;
 };
 const mainLinks: navBarLink[] = [
   {
     title: "Dashboard",
     destination: PageLocation.Dashboard,
     icon: <LayoutDashboard size="22" />,
+    disabled: true,
   },
   {
     title: "Assessments",
     destination: PageLocation.Assessments,
     icon: <Bookmark size="22" />,
+    disabled: true,
   },
   {
     title: "Study Plan",
@@ -45,6 +41,7 @@ const mainLinks: navBarLink[] = [
     title: "Community",
     destination: PageLocation.Error,
     icon: <People size="22" />,
+    disabled: true,
   },
 ];
 
@@ -56,45 +53,35 @@ const utilityLinks: navBarLink[] = [
 function NavigationBar() {
   const { pathname } = useLocation();
   return (
-    <NavigationMenu orientation="vertical" className="2xl:w-[328px] w-[250px] ">
-      <NavigationMenuList>
-        <div className="flex flex-col pt-20 pb-[150px] gap-11">
-          {mainLinks.map((link, index) => {
-            const onPage = pathname === link.destination;
-            return (
-              <NavigationMenuItem key={index} className="flex w-full">
-                {onPage && (
-                  <div className="border-2 border-white rounded-r-md" />
-                )}
-                <NavigationMenuLink
-                  asChild
-                  className={`flex gap-5 pl-7 ${
-                    onPage ? "text-white" : "text-secondary"
-                  }`}
-                >
-                  <Link to={link.destination}>
-                    {link.icon}
-                    {link.title}
-                  </Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            );
-          })}
-        </div>
-        <hr />
-        <div className="flex flex-col pt-12 pl-12 gap-11">
-          {utilityLinks.map((link, index) => (
-            <NavigationMenuItem key={index}>
-              <NavigationMenuLink asChild>
-                <Link to={link.destination} key={index}>
-                  {link.title}
-                </Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          ))}
-        </div>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <aside className="2xl:w-[328px] w-[250px] bg-violet-darker">
+      <div className="flex flex-col h-[80%] pt-20 gap-11">
+        {mainLinks.map((link, index) => {
+          const onPage = pathname === link.destination;
+          return (
+            <div key={index} className="flex w-full">
+              {onPage && <div className="border-2 border-white rounded-r-md" />}
+              <Link
+                to={link.destination}
+                className={`flex gap-5 pl-7 ${
+                  !onPage && "text-violet-inactive"
+                }`}
+              >
+                {link.icon}
+                {`${link.title} ${link.disabled ? "(Coming Soon)" : ""}`}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+      <div className="flex flex-col pt-12 pl-12 gap-11">
+        {utilityLinks.map((link, index) => (
+          <Link to={link.destination} key={index}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </aside>
   );
 }
 
