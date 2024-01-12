1befdb1120ef99808741f2468dc364d31c871110
Ke Duong <kelyduong@gmail.com>
2024-01-13 01:15:10 +1100

    (fe) small styling on quiz header

--- client/src/pages/Quiz/Structure/QuizHeader.tsx
+++ client/src/pages/Quiz/Structure/QuizHeader.tsx
@@ -1,4 +1,4 @@
-const quizMetaDataStyles = "flex flex-col gap-1 md:flex-row text-center";
+const quizMetaDataStyles = "flex flex-col gap-3 md:flex-row text-center";
 import { useQuizState } from "../../../states/Quiz.state";
 import { convertArrayIndexToQuestionIndex } from "../commonQuizFunctions";
 
@@ -11,17 +11,19 @@ const QuizHeader = () => {
   return (
     <div className="flex flex-col justify-between gap-6 md:flex-row">
       <div className="flex items-center justify-between w-full">
-        <div className={quizMetaDataStyles}>
-          <span>Subject</span>
-          <span>{quizMetaData?.subject}</span>
-        </div>
-        <div className={quizMetaDataStyles}>
+        <p className={quizMetaDataStyles}>
+          {quizMetaData?.mode}
+          {"  "} &gt; {"  "}
+          {quizMetaData?.subject}{" "}
+          {quizMetaData?.topics && `${quizMetaData.topics} >`}
+        </p>
+        <h3 className={quizMetaDataStyles}>
           <span>Questions</span>
           <span>
             {convertArrayIndexToQuestionIndex(currentQuestionIndex)} of{" "}
             {quizMetaData?.numberOfQuestions}
           </span>
-        </div>
+        </h3>
         {/* <div className="items-center hidden md:flex">
           <Timer1 size={IconSize.medium} /> {millisToMinutesAndSeconds(0)}
         </div> */}
