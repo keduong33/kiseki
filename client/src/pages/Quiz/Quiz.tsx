22fee268cb55fe4c23dc5a54226db19855cc55a0
Ke Duong <kelyduong@gmail.com>
2024-01-12 22:55:33 +1100

    (fe) remove question navigation from quiz

--- client/src/pages/Quiz/Quiz.tsx
+++ client/src/pages/Quiz/Quiz.tsx
@@ -1,29 +1,14 @@
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { Button } from "../../components/shadcn/ui/button";
 import { useQuizState } from "../../states/Quiz.state";
 import AnswerOptions from "./Structure/AnswerOptions";
 import Question from "./Structure/Question";
 import QuestionNavigation from "./Structure/QuestionNavigation";
 import QuizHeader from "./Structure/QuizHeader";
-import { SubmitQuizButton } from "./SubmitQuizButton";
 
 function Quiz() {
-  const [questions, currentQuestionIndex, setCurrentQuestionIndex] =
-    useQuizState((state) => [
-      state.questions,
-      state.currentQuestionIndex,
-      state.setCurrentQuestionIndex,
-    ]);
-
-  const prevQuestion = () => {
-    if (currentQuestionIndex > 0)
-      setCurrentQuestionIndex(currentQuestionIndex - 1);
-  };
-
-  const nextQuestion = () => {
-    if (currentQuestionIndex < questions.length - 1)
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-  };
+  const [questions, currentQuestionIndex] = useQuizState((state) => [
+    state.questions,
+    state.currentQuestionIndex,
+  ]);
 
   const currentQuestion = questions[currentQuestionIndex];
 
@@ -42,20 +27,6 @@ function Quiz() {
               options={currentQuestion.options}
               optionImageUrls={currentQuestion.optionImageUrls}
             />
-          </div>
-
-          <div className="flex justify-between w-full gap-3 md:justify-center">
-            <Button variant="outline" size="icon" onClick={prevQuestion}>
-              <ChevronLeft className="w-4 h-4" />
-            </Button>
-
-            <SubmitQuizButton
-              disabled={currentQuestionIndex !== questions.length - 1}
-            />
-
-            <Button variant="outline" size="icon" onClick={nextQuestion}>
-              <ChevronRight className="w-4 h-4" />
-            </Button>
           </div>
         </div>
       )}
