4c38d0f73fa23ec195b951de69668facefc91d48
Ke Duong <kelyduong@gmail.com>
2024-01-12 21:55:28 +1100

    (fe) remove select component

--- client/src/pages/Quiz/Structure/QuizHeader.tsx
+++ client/src/pages/Quiz/Structure/QuizHeader.tsx
@@ -1,30 +1,12 @@
 const quizMetaDataStyles = "flex flex-col gap-1 md:flex-row text-center";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../../../components/shadcn/ui/select";
 import { useQuizState } from "../../../states/Quiz.state";
-import { convertArrayIndexToQuestionIndex } from "../Quiz";
+import { convertArrayIndexToQuestionIndex } from "../commonQuizFunctions";
 
 const QuizHeader = () => {
-  const [
-    questions,
-    currentQuestionIndex,
-    setCurrentQuestionIndex,
-    quizMetaData,
-  ] = useQuizState((state) => [
-    state.questions,
+  const [currentQuestionIndex, quizMetaData] = useQuizState((state) => [
     state.currentQuestionIndex,
-    state.setCurrentQuestionIndex,
     state.quizMetaData,
   ]);
-
-  const changeQuestion = (newQuestionIndex: string) => {
-    setCurrentQuestionIndex(Number(newQuestionIndex));
-  };
 
   return (
     <div className="flex flex-col justify-between gap-6 md:flex-row">
@@ -43,23 +25,6 @@ const QuizHeader = () => {
         {/* <div className="items-center hidden md:flex">
           <Timer1 size={IconSize.medium} /> {millisToMinutesAndSeconds(0)}
         </div> */}
-      </div>
-      <div className="flex w-full gap-1">
-        <Select
-          onValueChange={changeQuestion}
-          value={currentQuestionIndex.toString()}
-        >
-          <SelectTrigger className="w-full">
-            <SelectValue />
-          </SelectTrigger>
-          <SelectContent className="overflow-y-auto h-[200px]">
-            {questions.map((question, index) => (
-              <SelectItem value={index.toString()} key={index}>
-                Question {convertArrayIndexToQuestionIndex(index)}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
       </div>
     </div>
   );
