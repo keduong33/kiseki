8c28020bfcf6abea149c57b63e04429b0c3e32f0
Ke Duong <kelyduong@gmail.com>
2024-01-21 01:11:54 +1100

    (fe) display topic-subtopic then skill

--- client/src/pages/StudyPlan/StudyCard.tsx
+++ client/src/pages/StudyPlan/StudyCard.tsx
@@ -5,14 +5,14 @@ import { Card } from "../../components/shadcn/ui/card";
 import type { ToStudy } from "./StudyPlan";
 
 function StudyCard({ subject, topic, subtopic, skill }: ToStudy) {
-  const toStudy =
-    topic + `${subtopic ? `-${subtopic}` : ""}` + `${skill ? `-${skill}` : ""}`;
   return (
     <Card className="w-[400px]">
       <div className="p-[30px] flex">
         <div className="w-full">
-          <h3>{subject}</h3>
-          <p className="text-sm text-[#8089C6]">{toStudy}</p>
+          <div className="flex gap-1">
+            <h3>{topic}</h3>-<h4>{subtopic}</h4>
+          </div>
+          <p className="text-sm text-[#8089C6]">{skill}</p>
         </div>
         <div className="flex items-center gap-3 place-self-center w-fit">
           <TickCircle size={IconSize.medium} />
