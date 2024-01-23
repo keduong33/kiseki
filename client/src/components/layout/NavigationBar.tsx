0dab8111dfdc085800c3ce14659c4beec99c6a72
Ke Duong <kelyduong@gmail.com>
2024-01-23 13:34:12 +1100

    (fe) use userButton instead of Profile link

--- client/src/components/layout/NavigationBar.tsx
+++ client/src/components/layout/NavigationBar.tsx
@@ -1,7 +1,9 @@
+import { SignInButton, UserButton, useUser } from "@clerk/clerk-react";
 import { Book1, Bookmark, Firstline, People } from "iconsax-react";
-import { LayoutDashboard } from "lucide-react";
+import { BookIcon, LayoutDashboard } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
 import { PageLocation } from "../../../../types/PageLocation";
+import { ProfileForm } from "../../pages/Profile/BuildProfile/ProfileForm";
 
 type navBarLink = {
   title: string;
@@ -49,12 +51,12 @@ const mainLinks: navBarLink[] = [
 ];
 
 const utilityLinks: navBarLink[] = [
-  { title: "Profile", destination: PageLocation.Profile },
   { title: "Settings", destination: PageLocation.Error },
 ];
 
 function NavigationBar() {
   const { pathname } = useLocation();
+  const { isSignedIn, user } = useUser();
   return (
     <aside className="2xl:w-[328px] w-[250px] bg-violet-darker flex flex-col overflow-auto pb-6">
       <div className="flex flex-col flex-1 pt-10 2xl:pt-20 gap-11">
@@ -83,6 +85,24 @@ function NavigationBar() {
       </div>
       <hr />
       <div className="flex flex-col pt-12 pl-12 gap-11">
+        {isSignedIn && (
+          <UserButton afterSignOutUrl={PageLocation.Dashboard}>
+            <UserButton.UserProfilePage
+              label="Additional Info"
+              url="additional-info"
+              labelIcon={<BookIcon size={16} />}
+            >
+              <ProfileForm user={user} textBlack />
+            </UserButton.UserProfilePage>
+            <UserButton.UserProfilePage label="account" />
+            <UserButton.UserProfilePage label="security" />
+          </UserButton>
+        )}
+        {!isSignedIn && (
+          <SignInButton>
+            <p className="cursor-pointer">Login</p>
+          </SignInButton>
+        )}
         {utilityLinks.map((link, index) => (
           <Link to={link.destination} key={index}>
             {link.title}
